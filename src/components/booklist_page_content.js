4098d22b7cc35d81d5d5f26dc8fd50a25d59c703
CalabresAC <carter.ash@gmail.com>
2018-05-28 10:15:59 -0400

    Main app routing done

--- src/components/booklist_page_content.js
+++ src/components/booklist_page_content.js
@@ -1,14 +1,24 @@
 import React from "react";
 import SearchButton from "./search_button.js";
+import BookShelf from "./booklist_shelf.js";
 
-const BookListPage = () => {
+const BookListPage = (props) => {
+  
+  filterBooks = (shelf) => {
+    const {books} = 
+  }
+  
   return (
     <div className="list-books">
       <div className="list-books-title">
         <h1>Ash Reads</h1>
       </div>
       <div className="list-books-content">
-        <span />
+        <div>
+          <BookShelf 
+          title="Currently Reading"
+          books={ this.filterBooks("currentlyReading") />
+        </div>
       </div>
       <SearchButton />
     </div>
